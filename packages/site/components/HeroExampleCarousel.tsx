23c52079bc3e98b76fee8a53a80151507828b056
natew <natewienert@gmail.com>
2022-03-25 20:35:50 -1000

    save

--- packages/site/components/HeroExampleCarousel.tsx
+++ packages/site/components/HeroExampleCarousel.tsx
@@ -1,63 +1,70 @@
 import { colorSchemes } from '@tamagui/theme-base'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { Button, H2, H3, Paragraph, Text, Theme, XStack, YStack } from 'tamagui'
+import { ScrollView } from 'react-native'
+import {
+  Button,
+  H2,
+  H3,
+  InteractiveContainer,
+  Paragraph,
+  Text,
+  Theme,
+  XStack,
+  YStack,
+  useTheme,
+  useThemeName,
+} from 'tamagui'
 
 import { Code, CodeInline } from './Code'
+import { useTint } from './ColorToggleButton'
 import { ContainerLarge } from './Container'
 import { MediaPlayer } from './MediaPlayer'
 
 const MediaPlayerDemo = ({ offset, index, ...props }: any) => {
   return (
-    <YStack
-      pos="absolute"
-      top={0}
-      left={0}
-      zi={100 - index}
-      px="$6"
-      mb={-220}
-      mr={-50}
-      py="$4"
-      className="faded-container"
-      x={index * 40}
-      y={index * 30}
-      // opacity={(4 - index) / 3}
-    >
+    <YStack zi={100 - index}>
       <MediaPlayer {...props} />
     </YStack>
   )
 }
 
-const MediaPlayerDemoStack = () => {
-  const [isHovering, setIsHovering] = useState(false)
-  const [i, setI] = useState(0)
+const MediaPlayerDemoStack = ({ name }: any) => {
+  const themeName = useThemeName()
+  const themes = [name ?? themeName, 'alt1', 'alt2', 'alt3']
+  const [active, setActive] = useState(0)
 
   return (
-    <YStack
-      pos="relative"
-      width={400}
-      height={80}
-      onHoverIn={() => setIsHovering(true)}
-      onHoverOut={() => setIsHovering(false)}
-    >
-      {[0, 1, 2, 3].map((demoIndex) => {
-        return (
-          <MediaPlayerDemo
-            key={demoIndex}
-            offset={i}
-            index={demoIndex + i}
-            {...(demoIndex && { alt: demoIndex })}
-          />
-        )
-      })}
-    </YStack>
+    <Theme name={name}>
+      <YStack ai="center" jc="center" space>
+        <InteractiveContainer>
+          {themes.map((name, i) => {
+            return (
+              <Button
+                onPress={() => setActive(i)}
+                theme={active === i ? 'active' : null}
+                key={i}
+                borderRadius="$0"
+                fontWeight={active === i ? '700' : '400'}
+              >
+                {name}
+              </Button>
+            )
+          })}
+        </InteractiveContainer>
+
+        <Theme name={active == 0 ? null : themes[active]}>
+          <MediaPlayerDemo />
+        </Theme>
+      </YStack>
+    </Theme>
   )
 }
 
 export function HeroExampleCarousel() {
   return (
     <YStack>
-      <ContainerLarge position="relative">
+      <ContainerLarge space="$6" position="relative">
         <YStack zi={1} space="$2">
           <H2 als="center">Truly flexible themes</H2>
           <H3 theme="alt2" als="center" fow="400">
@@ -66,19 +73,14 @@ export function HeroExampleCarousel() {
         </YStack>
 
         {/* <ScrollView horizontal showsHorizontalScrollIndicator={false}> */}
-        <XStack mb={220}>
+        <XStack space="$6">
           {[{ name: null }, ...colorSchemes.slice(2, 5)].map(({ name }, index) => {
-            return (
-              <Theme key={name} name={name}>
-                <MediaPlayerDemoStack />
-              </Theme>
-            )
+            return <MediaPlayerDemoStack key={name} name={name} />
           })}
         </XStack>
-
         {/* </ScrollView> */}
 
-        <YStack ai="center" als="center" maxWidth={480} space="$2">
+        <YStack mt="$3" ai="center" als="center" maxWidth={480} space="$2">
           {/* <H4 size="$8">Nest sub-themes infinitely</H4> */}
 
           <Paragraph mb="$3" ta="center" size="$5">
@@ -86,18 +88,15 @@ export function HeroExampleCarousel() {
             <br />
             Sprinkle a few <span className="rainbow">color alts</span> for each.
             <br />
-            Below that, add a <Text o={0.66}>few</Text> <Text o={0.5}>different</Text>{' '}
+            For each color, a <Text o={0.66}>few</Text> <Text o={0.5}>different</Text>{' '}
             <Text o={0.33}>shades</Text>.
             <br />
-            At any level, customize <CodeInline size="$4">&lt;Button /&gt;</CodeInline> or{' '}
-            <CodeInline size="$4">&lt;Input /&gt;</CodeInline>.
-            <br />
-            Nested themes - as easy as <CodeInline size="$4">dark_red_shade2_button</CodeInline>
+            For each of those a custom <CodeInline size="$4">&lt;Button /&gt;</CodeInline>.
           </Paragraph>
 
           <Link href="/docs/intro/themes" passHref>
             <Button theme="blue" tag="a">
-              Theme Docs &raquo;
+              Learn how themes work &raquo;
             </Button>
           </Link>
         </YStack>
