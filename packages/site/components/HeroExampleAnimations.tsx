f29e6caf8a7eacc8210dd9b7ab17b65936641c8e
natew <natewienert@gmail.com>
2022-03-26 23:17:13 -1000

    save

--- packages/site/components/HeroExampleAnimations.tsx
+++ packages/site/components/HeroExampleAnimations.tsx
@@ -1,22 +1,66 @@
-import { colorSchemes } from '@tamagui/theme-base'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { Button, H2, H3, Paragraph, Text, Theme, XStack, YStack } from 'tamagui'
+import { ScrollView } from 'react-native'
+import { Button, H2, H3, Paragraph, Separator, Square, XStack, YStack } from 'tamagui'
 
-import { Code, CodeInline } from './Code'
 import { ContainerLarge } from './Container'
-import { MediaPlayer } from './MediaPlayer'
 
 export function HeroExampleAnimations() {
   return (
     <YStack>
-      <ContainerLarge position="relative">
+      <ContainerLarge position="relative" space="$6">
         <YStack zi={1} space="$2">
           <H2 als="center">First class animations</H2>
           <H3 theme="alt2" als="center" fow="400">
             Swappable animation drivers for every platform.
           </H3>
         </YStack>
+
+        <XStack w="100%" theme="alt1" br="$6" overflow="hidden" bc="$background" h={400}>
+          <YStack ai="center" jc="center" f={2}>
+            {/* @ts-ignore */}
+            <Square size={100} bc="$color" br="$6" />
+          </YStack>
+          <Separator vertical />
+          <YStack f={1}>
+            <ScrollView>
+              {[
+                { name: 'Bouncy', description: 'A bouncy spring', animation: 'bouncy' },
+                { name: 'Lazy', description: 'A lazy, straightforward spring', animation: 'lazy' },
+                {
+                  name: 'Quick',
+                  description: 'A quick, straightforward spring',
+                  animation: 'quick',
+                },
+              ].map((item) => {
+                return (
+                  <YStack
+                    key={item.name}
+                    px="$3"
+                    py="$1"
+                    cursor="pointer"
+                    hoverStyle={{
+                      bc: '$backgroundHover',
+                    }}
+                  >
+                    <Paragraph cursor="inherit" size="$4" fontWeight="800">
+                      {item.name}
+                    </Paragraph>
+                    <Paragraph cursor="inherit" theme="alt2">
+                      {item.description}
+                    </Paragraph>
+                  </YStack>
+                )
+              })}
+            </ScrollView>
+
+            <Separator />
+
+            <YStack p="$4" ai="center" jc="$center">
+              <Button theme="blue" size="$6">
+                Run
+              </Button>
+            </YStack>
+          </YStack>
+        </XStack>
       </ContainerLarge>
     </YStack>
   )
--- packages/site/pages/index.tsx
+++ packages/site/pages/index.tsx
@@ -28,10 +28,8 @@ export default function Home() {
           <HeroExampleCarousel />
           <Divider />
           <HeroExampleAnimations />
-          <YStack space="$8" $sm={{ display: 'none' }}>
-            <Divider />
-            <HeroExampleCode />
-          </YStack>
+          <Divider />
+          <HeroExampleCode />
           <Divider />
           <Performance />
           <Divider />
@@ -49,7 +47,7 @@ function Performance() {
     <ContainerLarge position="relative">
       <YStack ai="center" zi={1} space="$4">
         <YStack ai="center" space="$2">
-          <H2>Really, really fast</H2>
+          <H2>... it's really fast</H2>
           <H3 theme="alt2" fow="400">
             3-10x speed, even with inline styling.
           </H3>
